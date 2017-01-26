37078d2162ad0a365112ca3e71be8ac1f727e008
Jinhong Lu <s1569197@brampton.inf.ed.ac.uk>
2017-01-26 13:38:33 +0000

    add firmware

---
+++ firmware/motorTest/motorTest.ino
@@ -0,0 +1,106 @@
+#include "SerialCommand.h"
+#include "SDPArduino.h"
+#include <Wire.h>
+#include <Arduino.h>
+
+#define FRONT 5
+#define RIGHT 3
+#define BACK 4
+#define LEFT 0
+#define KICKERS 1
+
+
+int run = 0;
+
+SerialCommand sCmd;
+
+
+
+void loop(){
+  motorControl(FRONT, 100);
+  motorControl(BACK, -100);
+  motorControl(LEFT, 100);
+  motorControl(RIGHT,-100);
+  delay(2500);
+  motorControl(FRONT, 100);
+  motorControl(BACK, -100);
+  motorControl(LEFT, 100);
+  motorControl(RIGHT,-100);
+  delay(2500);
+  motorControl(FRONT, 100);
+  motorControl(BACK, -100);
+  motorControl(LEFT, 100);
+  motorControl(RIGHT,-100);
+  delay(2500);
+  
+}
+
+
+void test(){
+  run = 1;
+}
+
+void dontMove(){
+  motorStop(FRONT);
+  motorStop(BACK);
+  motorStop(LEFT);
+  motorStop(RIGHT);
+}
+
+void spinmotor(){
+  int motor = atoi(sCmd.next());
+  int power = atoi(sCmd.next());
+  motorForward(motor, power);
+}
+
+void motorControl(int motor, int power){
+  if(power == 0){
+    motorStop(motor);
+  } else if(power > 0){
+    motorBackward(motor, power);
+  } else {
+    motorForward(motor, -power);
+  }
+}
+
+void rationalMotors(){
+  int front = atoi(sCmd.next());
+  int back  = atoi(sCmd.next());
+  int left  = atoi(sCmd.next());
+  int right = atoi(sCmd.next());
+  motorControl(FRONT, front);
+  motorControl(BACK, back);
+  motorControl(LEFT, left);
+  motorControl(RIGHT, right);
+}
+
+void pingMethod(){
+  Serial.println("pang");
+}
+
+void kicker(){
+  int type = atoi(sCmd.next());
+  if(type == 0){
+    motorStop(KICKERS);
+  } else if (type == 1){
+    motorForward(KICKERS, 100);
+  } else {
+    motorBackward(KICKERS, 100);
+  }
+}
+
+void completeHalt(){
+  motorAllStop();
+}
+
+void setup(){
+  sCmd.addCommand("f", dontMove); 
+  sCmd.addCommand("h", completeHalt); 
+  sCmd.addCommand("motor", spinmotor); 
+  sCmd.addCommand("r", rationalMotors); 
+  sCmd.addCommand("ping", pingMethod); 
+  sCmd.addCommand("kick", kicker); 
+  SDPsetup();
+  helloWorld();
+}
+
